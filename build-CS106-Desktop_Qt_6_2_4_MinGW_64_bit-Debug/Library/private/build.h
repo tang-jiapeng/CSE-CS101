fcc205c76d4485b78982922fbe8fb0d265a51bf2
Tangjp-wraith <83797617+Tangjp-wraith@users.noreply.github.com>
2022-09-07 16:32:42 +0800

    update

---
+++ build-CS106-Desktop_Qt_6_2_4_MinGW_64_bit-Debug/Library/private/build.h
@@ -0,0 +1,9 @@
+#ifndef SPL_BUILD_H
+#define SPL_BUILD_H
+
+#define SPL_VERSION "2021.1"
+#define SPL_INSTALL_LOCATION ""
+#define SPL_BUILD_DATE "Wed Sep 7 16:25:25 2022"
+#define SPL_BUILD_USER ""
+
+#endif
