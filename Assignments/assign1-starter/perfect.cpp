32abfd2ddecf1ce5a12d292a031ee14eabae9418
Tangjp-wraith <83797617+Tangjp-wraith@users.noreply.github.com>
2022-09-14 22:58:27 +0800

    update

---
+++ Assignments/assign1-starter/perfect.cpp
@@ -0,0 +1,196 @@
+/*
+ * TODO: remove and replace this file header comment
+ * This is a .cpp file you will edit and turn in.
+ * Remove starter comments and add your own
+ * comments on each function and on complex code sections.
+ */
+#include "console.h"
+#include <iostream>
+#include <cmath>
+#include "testing/SimpleTest.h"
+using namespace std;
+
+/* This function takes one argument `n` and calculates the sum
+ * of all proper divisors of `n` excluding itself. To find divisors
+ * a loop iterates over all numbers from 1 to n-1, testing for a
+ * zero remainder from the division.
+ *
+ * Note: long is a C++ type is a variant of int that allows for a
+ * larger range of values. For all intents and purposes, you can
+ * treat it like you would an int.
+ */
+long divisorSum(long n) {
+    long total = 0;
+    for (long divisor = 1; divisor < n; divisor++) {
+        if (n % divisor == 0) {
+            total += divisor;
+        }
+    }
+    return total;
+}
+
+/* This function takes one argument `n` and returns a boolean
+ * (true/false) value indicating whether or not `n` is perfect.
+ * A perfect number is a non-zero positive number whose sum
+ * of its proper divisors is equal to itself.
+ */
+bool isPerfect(long n) {
+    return (n != 0) && (n == divisorSum(n));
+}
+
+/* This function does an exhaustive search for perfect numbers.
+ * It takes one argument `stop` and searches the range 1 to `stop`,
+ * checking each number to see whether it is perfect and if so,
+ * printing it to the console.
+ */
+void findPerfects(long stop) {
+    for (long num = 1; num < stop; num++) {
+        if (isPerfect(num)) {
+            cout << "Found perfect number: " << num << endl;
+        }
+        if (num % 10000 == 0) cout << "." << flush; // progress bar
+    }
+    cout << endl << "Done searching up to " << stop << endl;
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+long smarterSum(long n) {
+    /* TODO: Fill in this function. */
+    long total=0;
+    if(n==1){
+        return 0;
+    }
+    for(long div=1;div<=sqrt(n);div++){
+        if(n%div==0){
+            total+=div;
+            if(n%(n/div)==0&&(n/div)!=n&&(n/div)!=div) {
+                total+=(n/div);
+            }
+        }
+    }
+    return total;
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+bool isPerfectSmarter(long n) {
+    /* TODO: Fill in this function. */
+    return (n != 0) && (n == smarterSum(n));
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+void findPerfectsSmarter(long stop) {
+     /* TODO: Fill in this function. */
+    for (long num = 1; num < stop; num++) {
+        if (isPerfectSmarter(num)) {
+            cout << "Found perfect number: " << num << endl;
+        }
+        if (num % 10000 == 0) cout << "." << flush; // progress bar
+    }
+    cout << endl << "Done searching up to " << stop << endl;
+}
+
+/* TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+
+bool isPrime(long n){
+    return (smarterSum(n)==1);
+}
+
+long findNthPerfectEuclid(long n) {
+    /* TODO: Fill in this function. */
+    long k=1,total=0;
+    while(true){
+        long m=pow(2,k)-1;
+        if(isPrime(m)){
+            total++;
+            if(total==n) {
+                return (pow(2,k)-1)*pow(2,k-1);
+            }
+        }
+        k++;
+    }
+    return 0;
+}
+
+
+/* * * * * * Test Cases * * * * * */
+
+/* Note: Do not add or remove any of the PROVIDED_TEST tests.
+ * You should add your own STUDENT_TEST tests below the
+ * provided tests.
+ */
+
+PROVIDED_TEST("Confirm divisorSum of small inputs") {
+    EXPECT_EQUAL(divisorSum(1), 0);
+    EXPECT_EQUAL(divisorSum(6), 6);
+    EXPECT_EQUAL(divisorSum(12), 16);
+}
+
+PROVIDED_TEST("Confirm 6 and 28 are perfect") {
+    EXPECT(isPerfect(6));
+    EXPECT(isPerfect(28));
+}
+
+PROVIDED_TEST("Confirm 12 and 98765 are not perfect") {
+    EXPECT(!isPerfect(12));
+    EXPECT(!isPerfect(98765));
+}
+
+PROVIDED_TEST("Test oddballs: 0 and 1 are not perfect") {
+    EXPECT(!isPerfect(0));
+    EXPECT(!isPerfect(1));
+}
+
+PROVIDED_TEST("Confirm 33550336 is perfect") {
+    EXPECT(isPerfect(33550336));
+}
+
+PROVIDED_TEST("Time trials of findPerfects on doubling input sizes") {
+    TIME_OPERATION(10000, findPerfects(10000));
+    TIME_OPERATION(20000, findPerfects(20000));
+    TIME_OPERATION(40000, findPerfects(40000));
+}
+
+// TODO: add your student test cases here
+
+STUDENT_TEST("Time trials of findPerfects on doubling input sizes by student"){
+    TIME_OPERATION(20000,findPerfects(20000));
+    TIME_OPERATION(40000,findPerfects(40000));
+    TIME_OPERATION(80000,findPerfects(80000));
+    TIME_OPERATION(160000,findPerfects(160000));
+}
+
+STUDENT_TEST("Check negative numbers aren't Perfect"){
+    EXPECT(!isPerfect(-1));
+    EXPECT(!isPerfect(-10));
+    EXPECT(!isPerfect(-6));
+}
+
+STUDENT_TEST("Check smarterSum is true"){
+    EXPECT_EQUAL(smarterSum(25),divisorSum(25));
+    EXPECT_EQUAL(smarterSum(6),divisorSum(6));
+    EXPECT_EQUAL(smarterSum(1),divisorSum(1));
+}
+
+STUDENT_TEST("Time trials of findPerfectsSmarter on doubling input sizes by student"){
+    TIME_OPERATION(600000,findPerfectsSmarter(600000));
+    TIME_OPERATION(1200000,findPerfectsSmarter(1200000));
+    TIME_OPERATION(2400000,findPerfectsSmarter(2400000));
+    TIME_OPERATION(4800000,findPerfectsSmarter(4800000));
+}
+
+
+STUDENT_TEST("Check findNthPerfectEuclid is true"){
+    EXPECT(isPerfect(findNthPerfectEuclid(1)));
+    EXPECT(isPerfect(findNthPerfectEuclid(2)));
+    EXPECT(isPerfect(findNthPerfectEuclid(3)));
+    EXPECT(isPerfect(findNthPerfectEuclid(5)));
+
+}
---
+++ Assignments/assign1-starter/short_answer.md
@@ -0,0 +1,73 @@
+Perfect Numbers
+---------------
+
+> Q1. Roughly how long did it take your computer to do the search? How many perfect numbers were found and what were they?
+
+A1. 运行时间大约3s；找到了4个，分别为：6、28、496、8128
+
+> Q2. Record the timing results for `findPerfects` that you observed into a table.
+
+A2.  STUDENT_TEST执行时间表格如下：
+
+| Size   | Time(s) |
+| ------ | ------- |
+| 20000  | 0.867   |
+| 40000  | 2.935   |
+| 80000  | 11.199  |
+| 160000 | 44.087  |
+
+> Q3. Does it take the same amount of work to compute `isPerfect` on the number 10 as it does on the number 1000? Why or why not? Does it take the same amount of work for `findPerfects` to search the range of numbers from 1-1000 as it does to search the numbers from 1000-2000? Why or why not?
+
+A3.
+1.`isPerfect`调用了 `divisorSum`，而 `divisorSum`的时间复杂度是$O(n)$，因此对于10和1000，搜索的范围不一样，做的工作不一样多。
+2.对于搜索范围为 1-1000 的数，`findPerfect`其工作量是否与 1000-2000 时不一样多，函数的复杂度近为$O(n^2)$，显然$O(\sum_{i=1000}^{2000} i^2)>>O(\sum_{i=1}^{1000} i^2)$
+
+> Q4. Extrapolate from the data you gathered and make a prediction: how long will it take `findPerfects` to reach the fifth perfect number?
+
+A4. 通过之前的数据推测大概是数据翻倍，搜索时间增长四倍，经过粗略计算，大概推断找到第五个数 `findPerfects`大约需要十几天。
+
+> Q5. Do any of the tests still pass even with this broken function? Why or why not?
+
+A5. 其他测试中有些测试依旧可以通过，有些测试不能通过。通过的测试函数有的并没有调用这个函数，不会受到影响；有一个测试用例调用了这个函数但是由于本身样例就是不是完全数，改了函数之后依旧满足不是完全数，因此也通过了测试。
+
+Q6. Describe the testing strategy you used for your test cases to confirm `smarterSum` is working correctly.
+
+A6.测试值为1，6，25；1是边界值，6是完全数，25是平方数。
+
+> Q7. Record your timing results for `findPerfectsSmarter` into a table.
+
+A7. STUDENT_TEST执行时间表格如下：
+
+| Size    | Time(s) |
+| ------- | ------- |
+| 600000  | 2.974   |
+| 1200000 | 7.445   |
+| 2400000 | 19.235  |
+| 4800000 | 51.764  |
+
+> Q8. Make a prediction: how long will `findPerfectsSmarter` take to reach the fifth perfect number?
+
+A8. 经过粗略计算，大概推断找到第五个数 `findPerfects`大约需要十几分钟。
+
+> Q9. Explain how you chose your specific test cases and why they lead you to be confident `findNthPerfectEuclid` is working correctly.
+
+A9. 我的测试用例是第1，2，3，5个完全数，本地电脑`long`类型最多测试到第5个完全数，第6个完全数位数为10，`long`类型溢出。
+
+Soundex
+-------
+
+> Q10. What is the Soundex code for "Angelou"? What is the code for your own surname?
+
+A10. TODO
+
+> Q11. Before writing any code, brainstorm your plan of attack and sketch how you might decompose the work into smaller tasks. Briefly describe your decomposition strategy.
+
+A11. TODO
+
+> Q12. Think about one or more examples of a class of names that the Soundex system might not work well for. Explain what this class of names is and why the system might incorrectly group them or mis-categorize one of the names.
+
+A12. TODO
+
+> Q13. Suppose you are a software engineer working for the U.S. government. You have been tasked with implementing a system that collects names as part of the Census survey and analyzes these names using a phonetic algorithm. Your boss suggests using Soundex and notes that the algorithm is only expected to work for ASCII-encoded strings, since supporting Unicode would require extra work and time. What would your response be and why? What representational harms might result from building a system that exclusively uses Soundex and/or ASCII?
+
+A13. TODO
