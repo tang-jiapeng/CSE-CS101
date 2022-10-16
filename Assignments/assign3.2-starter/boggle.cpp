61ce260ca23e5c16b01793e29f7df36850ef9f99
Tangjp-wraith <3081129260@qq.com>
2022-10-16 22:34:48 +0800

    update

---
+++ Assignments/assign3.2-starter/boggle.cpp
@@ -0,0 +1,134 @@
+#include <iostream>    // for cout, endl
+#include <string>      // for string class
+#include "backtracking.h"
+#include "gridlocation.h"
+#include "grid.h"
+#include "set.h"
+#include "lexicon.h"
+#include "testing/SimpleTest.h"
+using namespace std;
+
+
+int points(string str) {
+    return str.size()-3;
+}
+
+void findValidword(Grid<char>& board,Lexicon& lex,Grid<bool>& vis,int row,int col,string perfix,Set<string>& validwords){
+    if(!board.inBounds(row,col)||vis.get(row,col)){
+        return ;
+    }
+    perfix+=board.get(row,col);
+    if(!lex.containsPrefix(perfix)){
+        return ;
+    }
+    vis.set(row,col,true);
+    if(lex.contains(perfix)&&perfix.size()>3){
+        validwords.add(perfix);
+    }
+    for(int i=-1;i<=1;i++){
+        for(int j=-1;j<=1;j++){
+            findValidword(board,lex,vis,row+i,col+j,perfix,validwords);
+        }
+    }
+    vis.set(row,col,false);
+}
+
+int scoreBoard(Grid<char>& board, Lexicon& lex) {
+    Set<string>validwords;
+    Grid<bool>vis(board.numRows(),board.numCols(),false);
+    for(int i=0;i<board.numRows();i++){
+        for(int j=0;j<board.numCols();j++){
+            findValidword(board,lex,vis,i,j,"",validwords);
+        }
+    }
+    int scores=0;
+    for(auto str : validwords){
+        scores+=points(str);
+    }
+    return scores;
+}
+
+/* * * * * * Test Cases * * * * * */
+
+/* Test helper function to return shared copy of Lexicon. Use to
+ * avoid (expensive) re-load of word list on each test case. */
+static Lexicon& sharedLexicon() {
+    static Lexicon lex("res/EnglishWords.txt");
+    return lex;
+}
+
+PROVIDED_TEST("Load shared Lexicon, confirm number of words") {
+    Lexicon lex = sharedLexicon();
+    EXPECT_EQUAL(lex.size(), 127145);
+}
+
+PROVIDED_TEST("Test point scoring") {
+    EXPECT_EQUAL(points("and"), 0);
+    EXPECT_EQUAL(points("quad"), 1);
+    EXPECT_EQUAL(points("quint"), 2);
+    EXPECT_EQUAL(points("sextet"), 3);
+    EXPECT_EQUAL(points("seventh"), 4);
+    EXPECT_EQUAL(points("supercomputer"), 10);
+}
+
+PROVIDED_TEST("Test scoreBoard, board contains no words, score of zero") {
+    Grid<char> board = {{'B','C','D','F'}, //no vowels, no words
+                        {'G','H','J','K'},
+                        {'L','M','N','P'},
+                        {'Q','R','S','T'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 0);
+}
+
+PROVIDED_TEST("Test scoreBoard, board contains one word, score of 1") {
+    Grid<char> board = {{'C','_','_','_'},
+                        {'Z','_','_','_'},
+                        {'_','A','_','_'},
+                        {'_','_','R','_'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, alternate paths for same word, still score of 1") {
+    Grid<char> board = {{'C','C','_','_'},
+                        {'C','Z','C','_'},
+                        {'_','A','_','_'},
+                        {'R','_','R','_'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, small number of words in corner of board") {
+    Grid<char> board = {{'L','I','_','_'},
+                        {'M','E','_','_'},
+                        {'_','S','_','_'},
+                        {'_','_','_','_'}};
+    Set<string> words = {"SMILE", "LIMES", "MILES", "MILE", "MIES", "LIME", "LIES", "ELMS", "SEMI"};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()),  2 + 2 + 2 + 1 + 1 + 1 + 1 + 1 + 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, small number of words") {
+    Grid<char> board = {{'E','Z','R','R'},
+                        {'O','H','I','O'},
+                        {'N','J','I','H'},
+                        {'Y','A','H','O'}};
+    Set<string> words = { "HORIZON", "OHIA", "ORZO", "JOHN", "HAJI"};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 4 + 1 + 1 + 1 + 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, medium number of words") {
+    Grid<char> board = {{'O','T','H','X'},
+                        {'T','H','T','P'},
+                        {'S','S','F','E'},
+                        {'N','A','L','T'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 76);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, large number of words") {
+    Grid<char> board = {{'E','A','A','R'},
+                        {'L','V','T','S'},
+                        {'R','A','A','N'},
+                        {'O','I','S','E'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 234);
+}
